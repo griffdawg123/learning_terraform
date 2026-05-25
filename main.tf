d1189166d8fb8903d284faf45d4bdaabf9eec42b
Griffin <doylegriffin32@gmail.com>
2026-05-25 14:29:37 +1000

    fixed zones

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-southeast-2", "ap-southeast-1", "ap-northeast-1"]
+  azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
